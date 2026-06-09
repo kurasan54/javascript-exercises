bdebd4115ec8f8c0ce0cb61b73034e1aedeef58e
Muazzam Amran <41406014+kurasan54@users.noreply.github.com>
2026-06-09 20:33:24 +0800

    Added 11-tempConversion exercise solution

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let temp = fahrenheit;
+  fahrenheit = (temp - 32) * (5/9);
+  return temp = parseFloat(fahrenheit.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let temp = celsius;
+  celsius = (temp * (9/5)) + 32;
+  return temp = parseFloat(celsius.toFixed(1));
 };
 
 // Do not edit below this line

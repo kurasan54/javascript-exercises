1fa8be9abc8862912afbd01017d29d5c5d03ed23
Muazzam Amran <41406014+kurasan54@users.noreply.github.com>
2026-06-09 18:40:51 +0800

    Added 08-removeFromArray exercise solution

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+const newArray = [];
+
+for (let element of array)
+{
+    if (!args.includes(element)){
+        newArray.push(element);
+    }
+}
+    return newArray;
 };
 
 // Do not edit below this line

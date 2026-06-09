12be832b1c930c62ce90465e1a7ffc2e59b7ba9c
Muazzam Amran <41406014+kurasan54@users.noreply.github.com>
2026-06-09 16:06:35 +0800

    Added 06-repeatString exercise solution

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(word, amount) {
+    let totalString = "";
 
+    if (amount > 0)
+    {
+        for (let i = 0; i < amount; i++){
+        totalString += word;
+        }
+    }
+    else if (amount === 0){
+        totalString == "";
+    }
+    else{
+        totalString = "ERROR";
+    }
+    return totalString;
 };
 
 // Do not edit below this line
